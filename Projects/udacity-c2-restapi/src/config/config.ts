aff455e5ec7cf550327bf91ee8e2095ec17e296e
Moses Mwangi <ngigemoses1@outlook.com>
2022-08-22 17:03:02 +0300

    Add env variable

--- Projects/udacity-c2-restapi/src/config/config.ts
+++ Projects/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "udagramdevngigeU",
-    "password": "12345678qwerty",
-    "database": "udagramdevngigeDBName",
-    "host": "udagramdevngigedbid.c3lltcousxdr.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ngige-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
     "secret": " "
